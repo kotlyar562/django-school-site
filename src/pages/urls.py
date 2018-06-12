c730755ff54387a196fcd99300c165b4a5c9d15d
Andrey Kotlyar <kotlyar562@gmail.com>
2018-06-12 14:10:44 +0300

    Slug link - any chars

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 app_name = 'pages'
 urlpatterns = (
-    path('<slug:page_slug>/', views.page, name='page'),
+    re_path(r'^(?P<page_slug>.+)/$', views.page, name='page'),
 )
